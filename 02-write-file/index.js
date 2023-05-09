3202f03349c0c2558c43490a4d322b92ee3707d9
Slava Manzhela <slavamanzh@gmail.com>
2023-05-09 19:54:23 +0300

    fix: correct mistakes in task 2

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,14 +1,28 @@
-const fs = require("fs");
-const { stdin, stdout } = process;
+const fs = require('fs');
+const path = require('path');
+
+const { stdin, stdout, exit} = require('process');
+const absPath = path.join(__dirname, 'text.txt');
+
+const output = fs.createWriteStream(absPath);
 
 stdout.write('Введите что-нибудь\n');
+
 stdin.on('data', data => {
 
-  fs.appendFile("02-write-file/hello.txt", data, function(error){
-    let data = fs.readFileSync("02-write-file/hello.txt", "utf8");
-    console.log(data);
- });
-
+  if (data.toString().trim()==='exit') {
+    goodLuck();
+  }
+  output.write(data);
 });
+
+process.on('SIGINT', goodLuck);
+
+function goodLuck() {
+  stdout.write('\nУдачи')
+  exit();
+}
+
+
 
  
