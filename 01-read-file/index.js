f9ee94eddcb2e193c632ac681d3a11564d758e1c
Slava Manzhela <slavamanzh@gmail.com>
2023-05-11 18:25:02 +0300

    fix: solve null output problem in 1 task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -5,6 +5,7 @@ var stream = new fs.ReadStream('01-read-file/text.txt', {encoding: 'utf-8'});
 stream.on('readable', function(){
     var data = stream.read();
     console.log(data);
+    stream.close();
 });
 
  
